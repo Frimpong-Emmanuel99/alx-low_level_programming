0d2a05d5cfc04288a7698dff31d8582cf2816b73
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-09 16:55:45 +0000

    unix is basically a simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 
 /**
  * main - A program to print information without the use of printf and puts
- * Return 1
+ * Return: 1
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+stdout);
 	return (1);
 }
