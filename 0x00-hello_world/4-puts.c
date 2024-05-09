acaf84fede5f30e83e4cb66fec10b364953a4d04
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-09 13:59:22 +0000

    hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints put fuction
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
