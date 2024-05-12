783cc2b2479aa9b2ab3a4d48bd3028541a14b2e2
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-12 12:02:34 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for ( j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
