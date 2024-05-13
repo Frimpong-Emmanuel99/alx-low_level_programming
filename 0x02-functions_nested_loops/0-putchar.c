21e6f2e7ee3fab61a4883c5da1f4c143bf46f98d
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-13 13:11:12 +0000

    Multiple commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - A program to print _putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	for (i = 0; i < 7; i++)
+	{
+		_putchar(c[i]);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A program
+ *
+ */
+
+void print_alphabet(void)
+{
+
+}
