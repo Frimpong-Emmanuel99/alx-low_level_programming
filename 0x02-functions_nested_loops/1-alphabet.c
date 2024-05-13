a6030bda1eadeac431950d71119841a03113ba95
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-13 15:24:32 +0000

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - A program
+ * print_alphabet - Entry point
+ * Return: Aways return 0
  *
  */
 
 void print_alphabet(void)
 {
+	int i;
 
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
 }
