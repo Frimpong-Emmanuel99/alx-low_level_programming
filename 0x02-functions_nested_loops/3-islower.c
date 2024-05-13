2be736b795bb7daa739450c997f2ad4c7924281b
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-13 16:21:19 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: the character checker
+ * Return: 1 when successful and 0 when otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
