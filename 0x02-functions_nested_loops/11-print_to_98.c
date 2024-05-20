049eb3ac6fa82502758488f37234aa2a80b4039f
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-20 15:56:28 +0000

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * @n: the parameter checker
+ * Return:
+ */
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i);
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			if (i != 98)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *
+ * Return:
+ */
+
+void times_table(void)
+{
+
