75436d86df839c89d9c12031c3ce82f3877f2153
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-05-24 10:18:59 +0000

    correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,10 @@ void print_rev(char *s)
 {
 	int i, length;
 
-	for (length = 0; length != '\0'; i++)
+	for (length = 0; length(s) != '\0'; i++)
 
 	
-	for (i = _strlen(s) - 1; i >= 0; i--)
+	for (i = length(s) - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
