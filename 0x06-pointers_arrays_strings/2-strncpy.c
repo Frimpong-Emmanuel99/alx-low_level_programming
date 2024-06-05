92ee42907d33c30c5799dc1020d7f6a3ab75e764
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-06-05 13:59:49 +0000

    Copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * _strncpy - A function to copy string
+ * @dest: The destination pointer
+ * @src: The source pointer
+ * @n: The parameter to be checked
+ *
+ * Return: Return the destination pointer
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (n > 0 && *src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*ptr = '\0';
+		ptr++
+		n--;
+	}
+	return (dest)
+}
