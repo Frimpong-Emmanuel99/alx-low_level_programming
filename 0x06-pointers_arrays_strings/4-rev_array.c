0d198d755b72465d3805141690935501d8d74351
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-06-05 14:57:01 +0000

    A function to move the content of an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * reverse_array - A function to reverse content in an array
+ * @a: The array to be reversed
+ * @n: The content of the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+	int temp;
+
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+}
