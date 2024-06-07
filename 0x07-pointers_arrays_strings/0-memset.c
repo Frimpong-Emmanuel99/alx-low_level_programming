84d9cfab6890ab67d4f0532fe2d09e02b7ff56c5
Frimpong-Emmanuel99 <emfrimpong.ef@gmail.com>
2024-06-07 14:20:38 +0000

    A function to fill memory space

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _memset - A function to fill memory space
+ * @s:
+ * @b:
+ * @n:
+ *
+ * Return:
+ */
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+}
